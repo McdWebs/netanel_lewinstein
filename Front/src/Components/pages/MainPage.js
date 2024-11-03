fd5ac4cadac1c2c6fd8860a31078a6558865d9fc
MICKY1011 <michaelhalperin2@gmaiil.com>
2024-11-03 16:57:37 +0200

    initial commit

--- Front/src/Components/pages/MainPage.js
+++ Front/src/Components/pages/MainPage.js
@@ -11,7 +11,7 @@ const MainPage = () => {
     <div>
       <AccessibilityMenu />
       <CarouselImages />
-      <Collections />
+      {/* <Collections /> */}
       <Works />
       <FeedbackList />
       <SocialNetworks />
